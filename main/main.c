a6cc084e9fb53f67abce90f4581d0a22a355307d
Tudor3510 <tudorcioc@gmail.com>
2025-05-27 18:53:59 +0300

    Send consumers keys such as mute

--- main/main.c
+++ main/main.c
@@ -26,8 +26,8 @@ static const char *TAG = "example";
  * so we must define both report descriptors
  */
 const uint8_t hid_report_descriptor[] = {
-    TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(HID_ITF_PROTOCOL_KEYBOARD)),
-    TUD_HID_REPORT_DESC_MOUSE(HID_REPORT_ID(HID_ITF_PROTOCOL_MOUSE))
+    TUD_HID_REPORT_DESC_KEYBOARD(HID_REPORT_ID(1)),
+    TUD_HID_REPORT_DESC_CONSUMER(HID_REPORT_ID(2))
 };
 
 /**
@@ -87,69 +87,31 @@ void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_
 
 /********* Application ***************/
 
-typedef enum {
-    MOUSE_DIR_RIGHT,
-    MOUSE_DIR_DOWN,
-    MOUSE_DIR_LEFT,
-    MOUSE_DIR_UP,
-    MOUSE_DIR_MAX,
-} mouse_dir_t;
-
-#define DISTANCE_MAX        125
-#define DELTA_SCALAR        5
-
-static void mouse_draw_square_next_delta(int8_t *delta_x_ret, int8_t *delta_y_ret)
-{
-    static mouse_dir_t cur_dir = MOUSE_DIR_RIGHT;
-    static uint32_t distance = 0;
-
-    // Calculate next delta
-    if (cur_dir == MOUSE_DIR_RIGHT) {
-        *delta_x_ret = DELTA_SCALAR;
-        *delta_y_ret = 0;
-    } else if (cur_dir == MOUSE_DIR_DOWN) {
-        *delta_x_ret = 0;
-        *delta_y_ret = DELTA_SCALAR;
-    } else if (cur_dir == MOUSE_DIR_LEFT) {
-        *delta_x_ret = -DELTA_SCALAR;
-        *delta_y_ret = 0;
-    } else if (cur_dir == MOUSE_DIR_UP) {
-        *delta_x_ret = 0;
-        *delta_y_ret = -DELTA_SCALAR;
-    }
-
-    // Update cumulative distance for current direction
-    distance += DELTA_SCALAR;
-    // Check if we need to change direction
-    if (distance >= DISTANCE_MAX) {
-        distance = 0;
-        cur_dir++;
-        if (cur_dir == MOUSE_DIR_MAX) {
-            cur_dir = 0;
-        }
-    }
-}
-
 static void app_send_hid_demo(void)
 {
     // Keyboard output: Send key 'a/A' pressed and released
     ESP_LOGI(TAG, "Sending Keyboard report");
-    uint8_t keycode[6] = {HID_KEY_A, 0, 0, 0, 0, 0};
-    tud_hid_keyboard_report(HID_ITF_PROTOCOL_KEYBOARD, 0, keycode);
-    vTaskDelay(pdMS_TO_TICKS(10));
-    uint8_t keycode_release_all[6] = {0, 0, 0, 0, 0, 0};
-    tud_hid_keyboard_report(HID_ITF_PROTOCOL_KEYBOARD, 0, keycode_release_all);
+    uint8_t keycode[8] = {0, 0, HID_KEY_A, 0, 0, 0, 0, 0};
+    tud_hid_report(0x01, keycode, sizeof(keycode));
+    vTaskDelay(pdMS_TO_TICKS(100));
+    uint8_t keycode_release_all[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    tud_hid_report(0x01, keycode_release_all, sizeof(keycode_release_all));
 
-    // Mouse output: Move mouse cursor in square trajectory
-    ESP_LOGI(TAG, "Sending Mouse report");
-    int8_t delta_x;
-    int8_t delta_y;
-    for (int i = 0; i < (DISTANCE_MAX / DELTA_SCALAR) * 4; i++) {
-        // Get the next x and y delta in the draw square pattern
-        mouse_draw_square_next_delta(&delta_x, &delta_y);
-        tud_hid_mouse_report(HID_ITF_PROTOCOL_MOUSE, 0x00, delta_x, delta_y, 0, 0);
-        vTaskDelay(pdMS_TO_TICKS(20));
-    }
+
+    vTaskDelay(pdMS_TO_TICKS(100));
+
+    uint8_t volume_key[2] = {HID_USAGE_CONSUMER_MUTE, 0};
+
+    // Press
+    ESP_LOGI(TAG, "Consumer: Press Mute");
+    tud_hid_report(0x02, &volume_key, sizeof(volume_key));
+    vTaskDelay(pdMS_TO_TICKS(100));
+
+    // Release
+    ESP_LOGI(TAG, "Consumer: Release Mute");
+    volume_key[0] = 0;
+    tud_hid_report(0x02, &volume_key, sizeof(volume_key));
+
 }
 
 void app_main(void)
